3891c2dfeb72177906e02c1ee1fd045c0e57e549
HauShenn <jisoso996@gmail.com>
2024-12-22 16:03:14 +0800

    FIRST VERSION

--- lib/features/MedicationReminders.dart
+++ lib/features/MedicationReminders.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:healthcare_app/service/firestore_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:healthcare_app/service/notificationService.dart';
 
 import '../service/notificationService.dart';
